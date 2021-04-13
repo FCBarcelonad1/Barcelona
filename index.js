2bcdbc7fa35e7197a1a5428b15c1fec32dc3b210
user23132 <teocostin24@yahoo.com>
2021-04-13 14:22:09 +0300

    v2.0

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-var pressed=0;
+var pressed = 0;
 
-$(".arrow").click(function (e) { 
-    
-    if(pressed == 0)
-    {
+$(".arrow").click(function () {
+
+    if (pressed == 0) {
         $(".cups").slideDown();
         $(".cups").removeClass("invizibil");
-        
-        pressed=1;
-    }
-    else
-    {
+
+        pressed = 1;
+    } else {
         $(".cups").slideUp();
         $(".cups").addClass("invizibil");
-        pressed=0;
+        pressed = 0;
     }
 });
+
+var pressed1 = 0;
+
+$(".info").click(function () {
+
+    if (pressed1 == 0) {
+        $(".infoContact").slideDown();
+        $(".infoContact").removeClass("invizibil");
+        setTimeout(function(){
+            window.scrollBy(0, 1000);
+        }, 350); 
+
+        pressed1 = 1;
+    } else {
+        $(".infoContact").slideUp();
+        $(".infoContact").addClass("invizibil");
+        pressed1 = 0;
+    }
+});
+
